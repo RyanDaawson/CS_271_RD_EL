e2f5e970bc3edb08e454233774a934de27d85dcd
RyanDaawson <dawson_r1@denison.edu>
2024-01-25 23:04:54 -0500

    Update set.h

---
+++ set.h
@@ -0,0 +1,41 @@
+<<<<<<< Updated upstream
+=======
+#ifndef SET_H
+#define SET_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+template <class T>
+class Set
+{
+private:
+    struct Node
+    {
+        T item;
+        Node *next;
+    };
+
+    Node *head; // the pointer for the linked list
+
+public:
+    Set(void);
+    Set(const Set<T> &mySet);
+    ~Set(void);
+    void insert(const T &item, int index);
+    void remove(int index);
+    int cardinality(void) const;
+    bool isEmpty(void) const;
+    bool contains(const T &item) const;
+    bool operator==(void) const;
+    bool operator<=(void) const;
+    Set<T> operator+(const Set<T> &mySet) const;
+    Set<T> operator&(const Set<T> &mySet) const;
+    Set<T> operator-(const Set<T> &mySet) const;
+    void clear(void);
+}
+#include "set.cpp"
+#endif
+>>>>>>> Stashed changes
