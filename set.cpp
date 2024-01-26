606443b11f7cb6b13d86dd1f1202736a5fe8a734
evlenny <116751501+evlenny@users.noreply.github.com>
2024-01-25 22:36:18 -0500

    constructor

--- set.cpp
+++ set.cpp
@@ -6,4 +6,9 @@ template <class T>
 Set<T>::Set()
 {
     head = nullptr;
+    Node* currentNode = mySet.head;
+    while (currentNode != nullptr) {
+        insert(currentNode->item);
+        currentNode = currentNode->next;
+    }
 }
