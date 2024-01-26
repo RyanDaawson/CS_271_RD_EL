3ffda632611a1f79dd0aa43f74ee66602a70f140
RyanDaawson <dawson_r1@denison.edu>
2024-01-25 23:19:16 -0500

    New test
    Under T

--- set.cpp
+++ set.cpp
@@ -1,33 +1,38 @@
 #include set.h
 
 template <class T>
-Set<T>::Set() {
+Set<T>::Set()
+{
     head = nullptr
 }
 
 template <class T>
-Set<T>::Set(const Set<T>& mySet) {
+Set<T>::Set(const Set<T> &mySet)
+{
     head = nullptr;
-    Node* currentNode = mySet.head;
-    while (currentNode != nullptr) {
+    Node *currentNode = mySet.head;
+    while (currentNode != nullptr)
+    {
         insert(currentNode->item);
         currentNode = currentNode->next;
     }
 }
 
 template <class T>
-Set<T>::~Set() {
+Set<T>::~Set()
+{
     clear();
 }
 
 template <class T>
-void Set<T>::clear() {
-    Node* currentNode = head;
-    while (currentNode != nullptr) {
-        Node* nextNode = currentNode->next;
+void Set<T>::clear()
+{
+    Node *currentNode = head;
+    while (currentNode != nullptr)
+    {
+        Node *nextNode = currentNode->next;
         delete currentNode;
         currentNode = nextNode;
     }
     head = nullptr;
 }
-
